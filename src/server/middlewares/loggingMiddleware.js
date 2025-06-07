3519070bc377c55afc8ad6258caafa13765b6b8d
henrya0519 <henrya0519@hotmail.com>
2025-06-07 14:28:44 -0500

    fix (loggingMiddleware): save request headers correctly

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,20 +1,21 @@
 const loggingMiddleware = (db) =>
-    (req, res, next) => {
-        const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
-        const headers = JSON.stringify(req.headers);
-        const originalUrl = req.originalUrl;
-        // Persist this info on DB
-        console.log('ip: ', ip)
-        console.log('headers: ', headers)
-        console.log('originalUrl: ', originalUrl)
-        const data = db.logging.create({
-            ip:ip,
-            headers:headers,
-            action:originalUrl
+  async (req, res, next) => {
+    const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
+    const header = JSON.stringify(req.headers);
+    const originalUrl = req.originalUrl;
 
-        })
-        console.log('dataaaa: ', data)       
-        next();
+    try {
+      await db.logging.create({
+        ip,
+        header,
+        action: originalUrl,
+      });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('Error saving log:', err);
     }
+
+    next();
+  };
 
 module.exports = loggingMiddleware;
